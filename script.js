1a96d3d19f0591bb09c59b2482653b96476f88f3
rnasaks <rnasaks@gmail.com>
2024-05-13 21:25:04 +0600

    update

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ const hex = "0123456789ABCDEFG".split("");
 setInterval(() => {
     ctx.fillStyle = "rgba(0, 0, 0, .05)";
     ctx.fillRect(0, 0, w, h);
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "green";
     p.map((v, i) => {
         ctx.fillText(random(hex), i * 10, v);
         p[i] = v >= h || v > 50 + 10000 * Math.random() ? 0 : v + 10;
