339b2df4779d05dae5fce79fce3f9aee643a1d32
rnasaks <rnasaks@gmail.com>
2024-05-24 02:26:41 +0600

    update

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ navlist.addEventListener("click",()=>{
 
 // countdown
  // Set the discount validity date
- var discountValidityDate = new Date("May 21, 2024 23:59:59");
+ var discountValidityDate = new Date("May 30, 2024 23:59:59");
 
  // Display the discount validity date
  document.getElementById("discount-validity").innerText = "Discount Validity: " + discountValidityDate.toLocaleDateString();
